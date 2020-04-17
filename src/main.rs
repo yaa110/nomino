a991bb9074cc211682211b4e88a50ea56962075a
Navid <yaa110@gmail.com>
2020-04-17 07:22:34 +0430

    Rename files and create map of files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_std::fs;
 use async_std::prelude::*;
 use async_std::task;
 use atty::Stream;
@@ -5,6 +6,9 @@ use clap::{load_yaml, App};
 use colored::{self, Colorize};
 use nomino::errors::SourceError;
 use nomino::input::{Context, Formatter, Source};
+use prettytable::{cell, format, row, Table};
+use serde_json::map::Map;
+use serde_json::value::Value;
 use std::env::{args, set_current_dir};
 use std::error::Error;
 use std::path::Path;
@@ -41,6 +45,45 @@ async fn read_output(output: Option<&str>) -> Result<Option<Formatter>, Box<dyn 
     Ok(Some(Formatter::new(output.unwrap()).await?))
 }
 
+async fn map_files(
+    context: Context,
+    test_mode: bool,
+    need_map: bool,
+) -> Result<Option<Map<String, Value>>, Box<dyn Error>> {
+    let mut map_iter = context.into_iter().await?;
+    let mut map = if need_map { Some(Map::new()) } else { None };
+    let mut rename_result = true;
+    while let Some((input, output)) = map_iter.next().await {
+        if !test_mode {
+            rename_result = fs::rename(input.as_str(), output.as_str()).await.is_ok();
+        }
+        if rename_result && need_map {
+            map.as_mut().map(|m| m.insert(input, Value::String(output)));
+        }
+        rename_result = true;
+    }
+    Ok(map)
+}
+
+async fn print_map_table(map: Map<String, Value>) -> Result<(), Box<dyn Error>> {
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.set_titles(row!["Input".cyan(), "Output".cyan()]);
+    map.into_iter()
+        .enumerate()
+        .for_each(|(i, (input, output))| {
+            if let Value::String(output) = output {
+                if i % 2 == 0 {
+                    table.add_row(row![input.as_str(), output.as_str()]);
+                } else {
+                    table.add_row(row![input.as_str().purple(), output.as_str().purple()]);
+                }
+            }
+        });
+    table.printstd();
+    Ok(())
+}
+
 async fn run_app() -> Result<(), Box<dyn Error>> {
     let opts_format = load_yaml!("opts.yml");
     let opts = App::from_yaml(opts_format).get_matches();
@@ -57,12 +100,21 @@ async fn run_app() -> Result<(), Box<dyn Error>> {
         read_output(opts.value_of("output")).await?,
     )
     .await;
-    let mut map_iter = context.into_iter().await?;
-    while let Some((input, output)) = map_iter.next().await {
-        // TODO
+    let print_map = opts.is_present("print");
+    let generate_map = opts.value_of("generate");
+    let test_mode = opts.is_present("test");
+    let map = map_files(context, test_mode, print_map || generate_map.is_some()).await?;
+    if let Some(map_file) = generate_map {
+        fs::write(
+            map_file,
+            serde_json::to_vec_pretty(map.as_ref().unwrap())?.as_slice(),
+        )
+        .await?;
     }
-    // TODO if opts.is_present("generate")
-    // TODO if !opts.is_present("test")
+    if print_map {
+        colored::control::set_override(atty::is(Stream::Stdout));
+        print_map_table(map.unwrap()).await?;
+    }
     Ok(())
 }
 
